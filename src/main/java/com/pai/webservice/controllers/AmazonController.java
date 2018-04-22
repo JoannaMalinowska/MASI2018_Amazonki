d87cf1140c9511f0f9cdc5467c5a36fab8cd8007
Maciej Pluta <221648@edu.p.lodz.pl>
2018-04-22 21:59:12 +0200

    Added Service for Amazon (initial implementation).

---
+++ src/main/java/com/pai/webservice/controllers/AmazonController.java
@@ -0,0 +1,39 @@
+package com.pai.webservice.controllers;
+
+import am.ik.aws.apa.jaxws.ItemSearchResponse;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pai.webservice.model.ResponseObject;
+import com.pai.webservice.notifications.Notification;
+import com.pai.webservice.service.AmazonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping(value = "/amazon")
+public class AmazonController {
+
+    private static ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    private AmazonService amazonService;
+
+    @GetMapping(value = "")
+    public @ResponseBody
+    ResponseEntity getResult() {
+
+        amazonService.setSearchCategory("Books");
+        amazonService.prepareKeywordsForRequest(new ArrayList<String>(){{add("Java");}});
+
+        ItemSearchResponse response = amazonService.getResultFromRequest();
+
+        JsonNode returnData = mapper.valueToTree(response);
+        return new ResponseEntity<>(ResponseObject.createSuccess(Notification.TEST_GET_SUCCESS, returnData), HttpStatus.OK);
+    }
+
+}

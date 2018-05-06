f5ae1a9c5cd32544423e360c5dcc4e6c095d0f40
Maciej Grala <maciej.grala@makolab.com>
2018-05-06 11:45:43 +0200

    Added Watson controller

---
+++ src/main/java/com/pai/webservice/service/IWatsonService.java
@@ -0,0 +1,16 @@
+package com.pai.webservice.service;
+
+import am.ik.aws.apa.AwsApaRequester;
+import am.ik.aws.apa.jaxws.ItemSearchRequest;
+import am.ik.aws.apa.jaxws.ItemSearchResponse;
+
+import java.util.List;
+
+public interface IAmazonService {
+
+    AwsApaRequester createAmazonInstance();
+    ItemSearchRequest createAmazonRequest();
+    void setSearchCategory(String category);
+    void prepareKeywordsForRequest(List<String> keywords);
+    ItemSearchResponse getResultFromRequest ();
+}
